82f700505fe2126a9a43a90b63ed059d4d984f04
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-10-17 12:16:41 +0530

    Updates

---
+++ precache-manifest.ca41e599743e5b69c5806b0fae58f42e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434bdb4650e9532236bb713520b1a399",
+    "url": "/spark18-test/index.html"
+  },
+  {
+    "revision": "ec0dde62b7b8327a0466",
+    "url": "/spark18-test/static/css/main.806779ff.chunk.css"
+  },
+  {
+    "revision": "3a1ce9b6bb4a42ce00b7",
+    "url": "/spark18-test/static/js/2.dfcc55d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/spark18-test/static/js/2.dfcc55d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0dde62b7b8327a0466",
+    "url": "/spark18-test/static/js/main.f802b82f.chunk.js"
+  },
+  {
+    "revision": "63de42800d5aed442154",
+    "url": "/spark18-test/static/js/runtime-main.3969702f.js"
+  }
+]);
